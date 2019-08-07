e3fc8822f61b83f3b8db67ef39cb014a56b268e3
Nataliya R <n.rudenko@gmail.com>
2019-08-07 17:21:57 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,4 +8,10 @@ class SessionsController < ApplicationController
         redirect_to '/'
     end
     
+    private
+ 
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
+    
 end
