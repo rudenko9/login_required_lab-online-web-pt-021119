b2121ae2e2cd470608a8310d2e0b8eb6d3f5c96e
Nataliya R <n.rudenko@gmail.com>
2019-08-07 17:36:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root 'application#welcome'
+
+  get '/welcome' => 'application#welcome'
+  get '/login' => 'sessions#new'
+  get '/secret' => 'secrets#show'
+  post '/login' => 'sessions#create'
+  post '/destroy' => 'sessions#destroy'
   
 end
